abfe6d9846e22b5096c236f962e1f2c4fdeafc8a
MCal88 <caliendomrc@gmail.com>
2024-09-15 15:22:34 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -12,6 +12,15 @@ int mult(int a, int b){
     return (a * b);
 }
 
+int div(int a, int b){
+    if (b == 0){
+        printf("Cannot divide by zero");
+        return 0;
+    } 
+    else 
+        return (a / b);
+}
+
 int main(){
     return 0;
 }
