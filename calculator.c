f9ada611cba4a66cfbe25418f555f2980cd5d7aa
MCal88 <caliendomrc@gmail.com>
2024-09-15 16:04:15 -0500

    Added user interface for calculator operations

--- calculator.c
+++ calculator.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 int addition(int a, int b){
+    printf("You chose addition");
     return (a + b);
 }
 
 int subtraction(int a, int b){
+    printf("You chose subtraction");
     return (a - b);
 }
 
 int mult(int a, int b){
+    printf("You chose multiplication");
     return (a * b);
 }
 
 int div(int a, int b){
+    printf("You chose division");
     if (b == 0){
         printf("Cannot divide by zero");
         return 0;
@@ -22,5 +26,27 @@ int div(int a, int b){
 }
 
 int main(){
+    int a, b, op, result;
+    printf("Choose Operation:");
+    printf("\n1 : addition \n2 : subtraction \n3 : multiplication \n4 : division \n");
+    scanf("%d", &op);
+    printf("Choose two numbers: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    switch (op){
+        case 1:
+            result = addition(a,b);
+            break;
+        case 2:
+            result = subtraction(a,b);
+            break;
+        case 3:
+            result = mult(a,b);
+            break;
+        case 4:
+            result = div(a,b);
+            break;
+    }
+    printf("The result is: %d", result);
     return 0;
 }
